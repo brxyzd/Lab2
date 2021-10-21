bac81f768dd6c7e42420e6edd9b4f35a9bd529a7
Antonio Brkic <ABrkic@dspace.hr>
2021-10-21 18:16:26 +0200

    Add project files.

---
+++ Lab2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KompleksniBrojKlasa kbk1 = new KompleksniBrojKlasa(2, 4);
+
+            KompleksniBrojStruktura kbs1 = new KompleksniBrojStruktura(3, 5);
+            Console.WriteLine($"kbk1 = {kbk1}");
+            Console.WriteLine($"kbs1 = {kbs1}\n");
+            KompleksniBrojKlasa kbk2 = kbk1;
+
+            KompleksniBrojStruktura kbs2 = kbs1;
+
+            kbk2.RealniDio = 25;
+            kbk2.ImaginarniDio = 30;
+
+            kbs2.RealniDio = 50;
+            kbs2.ImaginarniDio = 55;
+
+            Console.WriteLine($"kbk1 = {kbk1}");
+            Console.WriteLine($"kbk2 = {kbk2}");
+            Console.WriteLine($"kbs1 = {kbs1}");
+            Console.WriteLine($"kbs2 = {kbs2}");
+            Console.ReadKey(true);
+        }
+    }
+}
